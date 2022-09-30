3cbe0429dc71c11e59bf804eeb90214506a5ec09
Fabrício Rosa de França <fabricio476@outlook.com.br>
2022-09-30 00:33:29 -0300

    aditado notas aluno

--- Nota/calculo.php
+++ Nota/calculo.php
@@ -8,39 +8,47 @@
       </title>
    </head>
    <body>
-      <center><strong>Controle de notas</strong></center> <br><br>   
+      <center><strong>Controle de Notas</strong></center> <br><br>   
 
 	  <?php // Inicio do PHP
 
-       $aluno = $_POST['aluno'];	
-	    $b1 = floatval($_POST['b1']); // floatval() converte string para float
+         // Recebendo os dados do formulario
+         $aluno = $_POST['aluno'];	
+	     $b1 = floatval($_POST['b1']); // floatval() converte string para float
 		 $b2 = floatval($_POST['b2']);
 		 $b3 = floatval($_POST['b3']);
 		 $b4 = floatval($_POST['b4']);
 
-
 		 $media = ($b1+$b2+$b3+$b4)/4; // Calculo da media
 
-
-       //strtoupper() converte para maiusculas
-		 echo "A média do aluno " . strtoupper($_POST['aluno']) . " é $media <br><br>";  // Mostra a media do aluno
-
-
-		 echo "A MEDIA DO ALUNO $aluno É $media <br><br>"; // Mostra a media do aluno (uma segunda forma)
-
+        ///strtoupper() converte para maiusculas
+		// echo "A média do aluno " . strtoupper($_POST['aluno']) . " é $media <br><br>";  // Mostra a media do aluno
+		// echo "A MEDIA DO ALUNO $aluno É $media <br><br>"; // Mostra a media do aluno (uma segunda forma)
+        
 		 if($media >= 7){
-		    echo "APROVADO";	 
+		    $resultado = "APROVADO";	 
 		 }
 		 else{
 		    if($media < 2.5){
-			   echo "REPROVADO";	
+				$resultado = "REPROVADO";	
 			}				
 			else{
-			   echo "RECUPERAÇÃO";	
+				$resultado = "RECUPERAÇÃO";	
 			}
-		 }
-	  ?> <!-- Fim do PHP -->
+		 } ?> <!-- Fim do PHP -->
+
+
+<center> <table border="1px"> 
+            <tr>
+			   <td><?php echo "A Media do Aluno ". strtoupper($aluno) ." é : $media <br><br>"; ?></td> 
+            </tr>
+			<tr>
+              <td style="text-align:center"><b><?php echo $resultado;?></b></td> <!-- Mostra o resultado do aluno -->
+		    </tr>
+
+
+		  </table>
 	  <br><br>
-	  <button onclick="history.back()"> Voltar </button> <!-- Botão para voltar à página anterior -->   
+	  <button onclick="history.back()"> Voltar </button></center> <!-- Botão para voltar à página anterior -->   
    </body>
 </html>
--- Nota/notas.php
+++ Nota/notas.php
@@ -3,37 +3,38 @@
 <html>
    <head>
       <meta charset="utf-8">
-      <title>
-	     Controle de notas
-      </title>
+
+      <title>Controle de notas</title>
    </head>
+
    <body>
-      <center><strong>Controle de notas</strong></center>
-      <form action = "calculo.php" method = "post">
-	     <table>
+      <center><strong>Controle de Notas</strong></center><br><br>
+
+	  <center>  <form action = "calculo.php" method = "post">
+	     <table border="1px">
 		    <tr>
-			   <td>Aluno</td>
+			   <td><b>Aluno :</b></td>
 			   <td><input type="text" name="aluno" size = "50" maxlength="50" style="text-transform:uppercase" required></td>
 			</tr>
 			<tr>
-			   <td>Primeiro bimestre</td>
-			   <td><input type="text" name="b1" size = "5" maxlength="5"></td>
+			   <td><b>Primeiro Bimestre :</b></td>
+			   <td><input type="text" name="b1" size = "50" maxlength="5"></td>
 			</tr>
 			<tr>
-			   <td>Segundo bimestre:</td>
-			   <td><input type="text" name="b2" size = "5" maxlength="5"></td>
+			   <td><b>Segundo Bimestre:</b></td>
+			   <td><input type="text" name="b2" size = "50" maxlength="5"></td>
 			</tr>
 			<tr>
-			   <td>Terceiro bimestre:</td>
-			   <td><input type="text" name="b3" size = "5" maxlength="5"></td>
+			   <td><b>Terceiro Bimestre:</b></td>
+			   <td><input type="text" name="b3" size = "50" maxlength="5"></td>
 			</tr>			
 			<tr>
-			   <td>Quarto bimestre:</td>
-			   <td><input type="text" name="b4" size = "5" maxlength="5"></td>
+			   <td><b>Quarto Bimestre:</b></td>
+			   <td><input type="text" name="b4" size = "50" maxlength="5"></td>
 			</tr>
 		 </table> <br><br>
 		 <input type="submit" value="Calcular media">
 		 <br><br>
-      </form> 	  	  
+      </form> 	  </center>	  
    </body>
 </html>
