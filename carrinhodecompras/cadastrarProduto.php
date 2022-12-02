421e93f10db99d300300674ecb2fbec83265a38a
Fabrício Rosa de França <fabricio476@outlook.com.br>
2022-12-02 08:15:23 -0300

    carrinho

---
+++ carrinhodecompras/cadastrarProduto.php
@@ -0,0 +1,5 @@
+
+// Campos: Nome do produto e valor de venda.
+Restrições: Todos os campos são obrigatórios. Não permitir o cadastro de produtos com
+o mesmo nome.
+Resultado: Cadastrar os produtos em um array.
